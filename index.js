685abda2d0674ee0a555a4fe4010f21d566eef2c
Toan Vo <toanalien@gmail.com>
2015-10-04 15:42:03 +0000

    add index add module

--- index.js
+++ index.js
@@ -1,20 +1,74 @@
+var getTileData = require('./modules/get-panorama-tiles');
+var panorama = require('./modules/get-panorama-by-location');
+var getPanoURL = require('./modules/get-pano-url');
+var Canvas = require('canvas');
+var async = require('async');
+var request = require('request');
+var Image = require('canvas').Image;
+var fs = require('fs');
+
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+function getPanoTileImages(id, zoom) {
+    if (!id) {
+        throw new Error('must specify panorama ID');
+    }
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+    zoom = (typeof zoom === 'number' ? zoom : 1) | 0; // integer value
+    if (zoom < 0 || zoom > 5) {
+        throw new Error('zoom is out of range, must be between 0 - 5 (inclusive)');
+    }
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+    var data = getTileData(zoom);
+    var images = [];
+    for (var y = 0; y < data.rows; y++) {
+        for (var x = 0; x < data.columns; x++) {
+            images.push({
+                url: getPanoURL(id, {
+                    x: x,
+                    y: y,
+                    zoom: zoom
+                }),
+                position: [x * data.tileWidth, y * data.tileHeight]
+            });
+        }
+    }
+    data.images = images;
+    return data;
+}
+
+function getImageFromURL(url) {
+    var img = new Image();
+    var abc;
+    request.get({
+        url: url,
+        encoding: null
+    }, function(err, res, body) {
+        img.src = body;
+        return body;
+    });
+}
+
+app.get('/json', function(req, res) {
+    if (req.query.lat && req.query.lng) {} else {
+        res.end('lat & lng required !');
+    }
+    var location = [req.query.lat, req.query.lng];
+    var zoom = parseInt(req.query.zoom);
+    panorama(location, function(err, result) {
+        if (err) throw err;
+        var data = getPanoTileImages(result.id, zoom);
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(JSON.stringify(data));
+    });
 });
+
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
---
+++ modules/get-panorama-by-location.js
@@ -0,0 +1,37 @@
+var nets = require('nets');
+var defined = require('defined');
+
+module.exports = function panoramaByLocation(location, opt, cb) {
+  if (!location || !Array.isArray(location)) {
+    throw new TypeError('must provide location [ lat, lng ]');
+  }
+
+  if (typeof opt === 'function') {
+    cb = opt;
+    opt = {};
+  }
+
+  opt = opt || {};
+
+  var radius = defined(opt.radius, 50);
+  var url = 'https://cbks0.google.com/cbk?cb_client=apiv3&authuser=0&hl=en&output=polygon&it=1%3A1&rank=closest&ll=' + location[0] + ',' + location[1] + '&radius=' + radius;
+  nets({
+    url: url,
+    json: true
+  }, function(err, res, body) {
+    if (err) {
+      return cb(bail(location));
+    }
+    if (!/^2/.test(res.statusCode)) {
+      return cb(new Error('http status code: ' + res.statusCode));
+    }
+    if (!body || !body.result || !body.result.length) {
+      return cb(bail(location));
+    }
+    cb(null, body.result[0]);
+  });
+};
+
+function bail(location) {
+  return new Error('could not find street view at: [ ' + location.join(', ') + ' ]');
+}
